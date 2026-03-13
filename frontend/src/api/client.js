122aee1cd02de8db38312b1fec568934e0858c91
Nigel Yeo <ar-nigel.yeo@rakuten.com>
2026-03-13 18:05:44 +0800

    fix: vite env var convention

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.API_URL
-  ? `${import.meta.env.API_URL}/api/v1`
+const baseURL = import.meta.env.VITE_API_URL
+  ? `${import.meta.env.VITE_API_URL}/api/v1`
   : "/api/v1";
 
 const client = axios.create({ baseURL });
